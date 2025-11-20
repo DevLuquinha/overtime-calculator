d22f33324fa03ac49893fd043d98ceb35e7ff41c
Dev Luquinha <lucasemmanuelrodrigues737@gmail.com>
2025-11-20 18:43:37 -0300

    FEAT: Create the OvertimeRecord class

---
+++ overtime-calculator/Models/OvertimeRecord.cs
@@ -0,0 +1,12 @@
+﻿namespace overtime_calculator.Models
+{
+    internal class OvertimeRecord
+    {
+        public DateTime Date { get; set; } 
+        public TimeSpan EntryTime { get; set; }
+        public TimeSpan ExitTime { get; set; }
+        public string? Description { get; set; }
+        public TimeSpan CalculatedOvertime { get; set; } // ExitTime - EntryTime - 1 hour lunch
+        public double DaysEquivalent { get; set; } // CalculatedOvertime / 6 hours
+    }
+}
