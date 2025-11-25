f7cf862b0893d1e09de69aa7e2e39404ae4b60af
Dev Luquinha <lucasemmanuelrodrigues737@gmail.com>
2025-11-25 20:16:57 -0300

    FEAT: Create the firsts properties

---
+++ overtime-calculator/ViewModels/OvertimeViewModel.cs
@@ -0,0 +1,49 @@
+﻿namespace overtime_calculator.ViewModels
+{
+    public class OvertimeViewModel : ViewModelBase
+    {
+        private DateTime _recordDate;
+        public DateTime RecordDate
+        {
+            get => _recordDate;
+            set 
+            { 
+                _recordDate = value; 
+                OnPropertyChanged();
+            }
+        }
+
+        private DateTime _entryTime;
+        public DateTime EntryTime
+        {
+            get => _entryTime;
+            set 
+            { 
+                _entryTime = value; 
+                OnPropertyChanged(); 
+            }
+        }
+
+        private DateTime _exitTime;
+        public DateTime ExitTime
+        {
+            get => _exitTime;
+            set 
+            { 
+                _exitTime = value; 
+                OnPropertyChanged(); 
+            }
+        }
+
+        private string? _description;
+        public string? Description
+        {
+            get => _description;
+            set 
+            { 
+                _description = value; 
+                OnPropertyChanged(); 
+            }
+        }
+    }
+}
