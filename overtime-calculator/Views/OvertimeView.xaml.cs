ee0db76b03e42e1726448566942b7494146530fb
Dev Luquinha <lucasemmanuelrodrigues737@gmail.com>
2025-11-25 20:18:22 -0300

    FEAT: Create the OvertimeView control

---
+++ overtime-calculator/Views/OvertimeView.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace overtime_calculator.Views
+{
+    /// <summary>
+    /// Interaction logic for OvertimeView.xaml
+    /// </summary>
+    public partial class OvertimeView : UserControl
+    {
+        public OvertimeView()
+        {
+            InitializeComponent();
+            DataContext = new ViewModels.OvertimeViewModel();
+        }
+    }
+}
