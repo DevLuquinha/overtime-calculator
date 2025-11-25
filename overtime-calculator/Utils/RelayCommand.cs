add92cc2b37f45080a9303651fb2c310e6985173
Dev Luquinha <lucasemmanuelrodrigues737@gmail.com>
2025-11-25 20:36:08 -0300

    FEAT: Update the RelayCommand class

--- overtime-calculator/Utils/RelayCommand.cs
+++ overtime-calculator/Utils/RelayCommand.cs
@@ -1,26 +1,26 @@
-﻿using System.Windows.Input;
+﻿using System;
+using System.Windows.Input;
 
 namespace overtime_calculator.Utils
 {
     public class RelayCommand : ICommand
     {
-        private readonly Action<object?> _execute;
-        private readonly Func<object?, bool>? _canExecute;
+        private readonly Action<object> _execute;
+        private readonly Predicate<object> _canExecute;
 
-        public event EventHandler? CanExecuteChanged
+        public RelayCommand(Action<object> execute, Predicate<object> canExecute = null)
         {
-            add { CommandManager.RequerySuggested += value; }
-            remove { CommandManager.RequerySuggested -= value; }
-        }
-
-
-        public RelayCommand(Action<object> execute, Func<object, bool> canExecute = null)
-        {
-            _execute = execute;
+            _execute = execute ?? throw new ArgumentNullException(nameof(execute));
             _canExecute = canExecute;
         }
-
-        public bool CanExecute(object? parameter) => _canExecute == null || _canExecute(parameter);
         public void Execute(object? parameter) => _execute(parameter);
+        public bool CanExecute(object? parameter) => _canExecute == null || _canExecute(parameter);
+
+
+        public event EventHandler? CanExecuteChanged;
+        public void RaiseCanExecuteChanged()
+        {
+            CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+        }
     }
 }
