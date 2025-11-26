625786c87e04a6e57facce0c1aad7df6468076db
Dev Luquinha <lucasemmanuelrodrigues737@gmail.com>
2025-11-25 21:12:52 -0300

    FEAT: Create the OvertimeRecordViewModel class

---
+++ overtime-calculator/ViewModels/OvertimeRecordViewModel.cs
@@ -0,0 +1,19 @@
+﻿using overtime_calculator.Utils;
+using overtime_calculator.Models;
+
+namespace overtime_calculator.ViewModels
+{
+    public class OvertimeRecordViewModel : ViewModelBase
+    {
+        private readonly OvertimeRecord _overtimeRecord;
+
+        public DateTime RecordDate => _overtimeRecord.Date;
+        public DateTime EntryTime => _overtimeRecord.EntryTime;
+        public DateTime ExitTime => _overtimeRecord.ExitTime;
+
+        public OvertimeRecordViewModel(OvertimeRecord overtimeRecord)
+        {
+            _overtimeRecord = overtimeRecord;
+        }
+    }
+}
