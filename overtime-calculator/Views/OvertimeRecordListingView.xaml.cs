7dc404581edd6c448131586862eea212061014e1
Dev Luquinha <lucasemmanuelrodrigues737@gmail.com>
2025-11-25 21:17:07 -0300

    FEAT: Create the OvertimeRecordListingView

---
+++ overtime-calculator/Views/OvertimeRecordListingView.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace overtime_calculator.Views
+{
+    public partial class OvertimeRecordListingView : UserControl
+    {
+        public OvertimeRecordListingView()
+        {
+            InitializeComponent();
+        }
+    }
+}
