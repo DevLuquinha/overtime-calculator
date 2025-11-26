9eaf6eaac77a66853700641d56802fda68b0c1ce
Dev Luquinha <lucasemmanuelrodrigues737@gmail.com>
2025-11-25 21:11:11 -0300

    FEAT: Update the OvertimeRecord Model

--- overtime-calculator/Models/OvertimeRecord.cs
+++ overtime-calculator/Models/OvertimeRecord.cs
@@ -2,23 +2,11 @@
 {
     public class OvertimeRecord
     {
-        public DateTime Date { get; } 
-        public TimeSpan EntryTime { get; }
-        public TimeSpan ExitTime { get; }
-        public string? Description { get; }
+        public DateTime Date { get; set; } 
+        public DateTime EntryTime { get; set; }
+        public DateTime ExitTime { get; set; }
+        public string? Description { get; set; }
         public TimeSpan CalculatedOvertime => ExitTime - EntryTime - TimeSpan.FromHours(1);
         public double DaysEquivalent => CalculatedOvertime.TotalHours / 6.0;
-
-        public OvertimeRecord(
-            DateTime date,
-            TimeSpan entryTime,
-            TimeSpan exitTime,
-            string? description)
-        {
-            Date = date;
-            EntryTime = entryTime;
-            ExitTime = exitTime;
-            Description = description;
-        }
     }
 }
