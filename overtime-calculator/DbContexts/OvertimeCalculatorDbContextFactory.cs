18b6e87995971d24a4222bc24fd7917427104a3c
Dev Luquinha <lucasemmanuelrodrigues737@gmail.com>
2025-11-27 18:26:35 -0300

    FEAT: Create the OvertimeCalculatorDbContextFactory

---
+++ overtime-calculator/DbContexts/OvertimeCalculatorDbContextFactory.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace overtime_calculator.DbContexts
+{
+    public class OvertimeCalculatorDbContextFactory
+    {
+        private readonly string _connectionString;
+
+        public OvertimeCalculatorDbContextFactory(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public OvertimeCalculatorDbContext CreateDbContext()
+        {
+            DbContextOptions options = new DbContextOptionsBuilder()
+                .UseSqlite(_connectionString)
+                .Options;
+
+            return new OvertimeCalculatorDbContext(options);
+        }
+    }
+}
