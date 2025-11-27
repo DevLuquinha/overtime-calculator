0a179882d9e4dae1073cd68a6ce837e51e5ec803
Dev Luquinha <lucasemmanuelrodrigues737@gmail.com>
2025-11-27 18:34:56 -0300

    FEAT: Implemente the DatabaseOvertimeRecordProvider class

--- overtime-calculator/Services/OvertimeRecordProviders/DatabaseOvertimeRecordProvider.cs
+++ overtime-calculator/Services/OvertimeRecordProviders/DatabaseOvertimeRecordProvider.cs
@@ -1,12 +1,39 @@
-﻿using overtime_calculator.Models;
+﻿using Microsoft.EntityFrameworkCore;
+using overtime_calculator.DbContexts;
+using overtime_calculator.Models;
+using overtime_calculator.Models.DTOs;
 
 namespace overtime_calculator.Services.OvertimeRecordProviders
 {
     public class DatabaseOvertimeRecordProvider : IOvertimeRecordProvider
     {
-        public Task<IEnumerable<OvertimeRecord>> GetAllOvertimeRecords()
+        private readonly OvertimeCalculatorDbContextFactory _dbContextFactory;
+        public DatabaseOvertimeRecordProvider(OvertimeCalculatorDbContextFactory dbContextFactory)
         {
-            throw new NotImplementedException();
+            _dbContextFactory = dbContextFactory;
+        }
+
+        public async Task<IEnumerable<OvertimeRecord>> GetAllOvertimeRecords()
+        {
+            using (OvertimeCalculatorDbContext context = _dbContextFactory.CreateDbContext())
+            {
+                IEnumerable<OvertimeRecordDTO> overtimeRecordsDtos = await context
+                    .OvertimeRecords
+                    .ToListAsync();
+
+                return overtimeRecordsDtos.Select(dto => ToOvertimeRecord(dto));
+            }
+        }
+
+        private static OvertimeRecord ToOvertimeRecord(OvertimeRecordDTO dto)
+        {
+            return new OvertimeRecord
+            {
+                Date = dto.Date,
+                EntryTime = dto.EntryTime,
+                ExitTime = dto.ExitTime,
+                Description = dto.Description
+            };
         }
     }
 }
