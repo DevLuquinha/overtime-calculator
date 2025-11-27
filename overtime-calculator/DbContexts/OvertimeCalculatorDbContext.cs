5d874268779c8656ad4038392a3e18934b187f8e
Dev Luquinha <lucasemmanuelrodrigues737@gmail.com>
2025-11-27 17:56:29 -0300

    FEAT: Create the OvertimeCalculatorDbContext

---
+++ overtime-calculator/DbContexts/OvertimeCalculatorDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using overtime_calculator.Models.DTOs;
+
+namespace overtime_calculator.DbContexts
+{
+    public class OvertimeCalculatorDbContext : DbContext
+    {
+        public DbSet<OvertimeRecordDTO> OvertimeRecords { get; set; }
+    }
+}
