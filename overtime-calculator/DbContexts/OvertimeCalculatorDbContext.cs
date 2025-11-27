46f1a3fb98df5bb877c64c8738f72d5371efa62b
Dev Luquinha <lucasemmanuelrodrigues737@gmail.com>
2025-11-27 18:16:09 -0300

    FEAT: Add Migrations Dir

--- overtime-calculator/DbContexts/OvertimeCalculatorDbContext.cs
+++ overtime-calculator/DbContexts/OvertimeCalculatorDbContext.cs
@@ -5,6 +5,11 @@ namespace overtime_calculator.DbContexts
 {
     public class OvertimeCalculatorDbContext : DbContext
     {
+        public OvertimeCalculatorDbContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+
         public DbSet<OvertimeRecordDTO> OvertimeRecords { get; set; }
     }
 }
