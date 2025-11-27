f6310813a166996a95885d95ec879a4508715ed7
Dev Luquinha <lucasemmanuelrodrigues737@gmail.com>
2025-11-27 17:55:46 -0300

    FEAT: Create the OvertimeRecordDTO

---
+++ overtime-calculator/Models/DTOs/OvertimeRecordDTO.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace overtime_calculator.Models.DTOs
+{
+    public class OvertimeRecordDTO
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public DateTime Date { get; set; }
+        public DateTime EntryTime { get; set; }
+        public DateTime ExitTime { get; set; }
+        public string? Description { get; set; }
+        public TimeSpan CalculatedOvertime { get; set; }
+    }
+}
