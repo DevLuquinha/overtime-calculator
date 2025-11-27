8e6963d332830b8258be3ccb54665975af9efd9a
Dev Luquinha <lucasemmanuelrodrigues737@gmail.com>
2025-11-27 18:37:18 -0300

    FEAT: Create the OvertimeRecordCreator Interface

---
+++ overtime-calculator/Services/OvertimeRecordCreators/IOvertimeRecordCreator.cs
@@ -0,0 +1,9 @@
+﻿using overtime_calculator.Models;
+
+namespace overtime_calculator.Services.OvertimeRecordCreators
+{
+    public interface IOvertimeRecordCreator
+    {
+        Task CreateOvertimeRecord(OvertimeRecord overtimeRecord);
+    }
+}
