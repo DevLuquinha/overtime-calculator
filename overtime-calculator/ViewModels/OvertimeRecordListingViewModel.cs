64fa0c0495cabd4bb9203eb9044ea35ce31356cc
Dev Luquinha <lucasemmanuelrodrigues737@gmail.com>
2025-11-27 17:28:29 -0300

    FEAT: Clear code of OvertimeRecordListingViewModel

--- overtime-calculator/ViewModels/OvertimeRecordListingViewModel.cs
+++ overtime-calculator/ViewModels/OvertimeRecordListingViewModel.cs
@@ -12,23 +12,6 @@ namespace overtime_calculator.ViewModels
         public OvertimeRecordListingViewModel()
         {
             _overtimeRecords = new ObservableCollection<OvertimeRecordViewModel>();
-
-            // For demonstration purposes, adding some dummy data
-            //_overtimeRecords.Add(new OvertimeRecordViewModel(new OvertimeRecord
-            //{
-            //    Date = DateTime.Now.AddDays(-1),
-            //    EntryTime = DateTime.Now.AddDays(-1).AddHours(17),
-            //    ExitTime = DateTime.Now.AddDays(-1).AddHours(20),
-            //    Description = "Worked on project X"
-            //}));
-
-            //_overtimeRecords.Add(new OvertimeRecordViewModel(new OvertimeRecord
-            //{
-            //    Date = DateTime.Now.AddDays(-2),
-            //    EntryTime = DateTime.Now.AddDays(-2).AddHours(18),
-            //    ExitTime = DateTime.Now.AddDays(-2).AddHours(21),
-            //    Description = "Client meeting"
-            //}));
         }
 
         /// <summary>
