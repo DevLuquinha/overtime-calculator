b4fe746a50d96d8bc82cab6b1fbb4e5171502ffc
Dev Luquinha <lucasemmanuelrodrigues737@gmail.com>
2025-11-27 18:44:12 -0300

    FEAT: Create DatabaseOvertimeRecordCreation

---
+++ overtime-calculator/Services/OvertimeRecordCreators/DatabaseOvertimeRecordCreation.cs
@@ -0,0 +1,39 @@
+﻿using overtime_calculator.DbContexts;
+using overtime_calculator.Models;
+using overtime_calculator.Models.DTOs;
+
+namespace overtime_calculator.Services.OvertimeRecordCreators
+{
+    public class DatabaseOvertimeRecordCreation : IOvertimeRecordCreator
+    {
+        private readonly OvertimeCalculatorDbContextFactory _dbContextFactory;
+        public DatabaseOvertimeRecordCreation(OvertimeCalculatorDbContextFactory dbContextFactory)
+        {
+            _dbContextFactory = dbContextFactory;
+        }
+
+        public async Task CreateOvertimeRecord(OvertimeRecord overtimeRecord)
+        {
+            using (OvertimeCalculatorDbContext context = _dbContextFactory.CreateDbContext())
+            {
+                OvertimeRecordDTO overtimeRecordDTO = ToOvertimeRecordDTO(overtimeRecord);
+
+                context.OvertimeRecords.Add(overtimeRecordDTO);
+                await context.SaveChangesAsync();
+            }
+        }
+
+        private OvertimeRecordDTO ToOvertimeRecordDTO(OvertimeRecord overtimeRecord)
+        {
+            return new OvertimeRecordDTO
+            {
+                Id = Guid.NewGuid(),
+                Date = overtimeRecord.Date,
+                EntryTime = overtimeRecord.EntryTime,
+                ExitTime = overtimeRecord.ExitTime,
+                Description = overtimeRecord.Description,
+                CalculatedOvertime = overtimeRecord.CalculatedOvertime
+            };
+        }
+    }
+}
